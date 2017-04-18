b14433c8a8644142995f12a017efee4345162430
Melissa Duran <mduran8@nmsu.edu>
2017-04-18 15:47:21 -0600

    Add existing file

--- IglooScore2.java
+++ IglooScore2.java
@@ -41,13 +41,14 @@ public int overallScore()
       s2 = score3;
    } else if (score2 < score1 && score2 < score3) {
       s1 = score1;
-      s2 = score2;
+      s2 = score3;
    } else if (score3 < score1 && score3 < score2) {
       s1 = score1;
       s2 = score2;
+
    } else {
-      s1 = 99;
-      s2 = 99;
+      s1 = score1 ;
+      s2 = score2 ;
    }
    s = s1 + s2;
    return s;
@@ -56,7 +57,7 @@ public int overallScore()
 public static void main(String args[])
 {
    int s1, s2, s3;
-   if (args==null || args.length != 3) {
+   if (args.length != 3) {
       System.err.println("Error: must supply three arguments!");
       return;
    }
